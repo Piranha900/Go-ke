979ec3fd5c881e7c0b25257205b14c8a282d087f
Piranha900 <gianfelice.marco90@gmail.com>
2019-06-19 14:40:22 +0200

    Reworking totale

--- src/dnd_def.go
+++ src/dnd_def.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/jung-kurt/gofpdf"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -10,7 +11,6 @@ import (
 	"os"
 	"strconv"
 	"time"
-	"github.com/jung-kurt/gofpdf"
 )
 
 //funzione di errore
@@ -44,108 +44,90 @@ type Datas struct {
 	Allineamento    []string     `json:"allineamento"`
 	Taglia          []string     `json:"taglia"`
 	Dio             []string     `json:"dio"`
-	NomePersonaggio [][][]string `json:"nomepersonaggio"`
+	NomePersonaggio [][][]string `json:"nomePersonaggio"`
 }
 
 var Conf Datas
 
-//crea la struttura per la scheda personaggio
-type Personaggio struct {
-	Utente          string
-	Razza           string
-	Genere          string
-	NomePersonaggio string
-	Allineamento    string
-	Taglia          string
-	Dio             string
-	Classe          string
-	Selection       []int
+//genera scheda del personaggio randomizzata
+func Genera(pm map[string]string, s []int) map[string]string {
+
+	selezioneNome := Conf.NomePersonaggio[s[0]][s[1]] // crea il nome del personaggio basandosi su razza e genere
+	rand.Seed(time.Now().UnixNano())
+
+	pm["Nome"] = selezioneNome[rand.Intn(len(selezioneNome))]
+	pm["Allineamento"] = Conf.Allineamento[rand.Intn(len(Conf.Allineamento))]
+	pm["Taglia"] = Conf.Taglia[rand.Intn(len(Conf.Taglia))]
+	pm["Classe"] = Conf.Classe[rand.Intn(len(Conf.Classe))]
+	pm["Divinità"] = Conf.Dio[rand.Intn(len(Conf.Dio))]
+
+	return pm
 }
 
-//genera scheda del personaggio randomizzata
-func (p *Personaggio) Genera() error {
-
-	selezioneNome := Conf.NomePersonaggio[p.Selection[1]][p.Selection[0]] //crea il nome del personaggio basandosi su razza e genere
-	rand.Seed(time.Now().UnixNano())
-	p.NomePersonaggio = selezioneNome[rand.Intn(len(selezioneNome))]
-	p.Allineamento = Conf.Allineamento[rand.Intn(len(Conf.Allineamento))]
-	p.Taglia = Conf.Taglia[rand.Intn(len(Conf.Taglia))]
-	p.Classe = Conf.Classe[rand.Intn(len(Conf.Classe))]
-	p.Dio = Conf.Dio[rand.Intn(len(Conf.Dio))]
-
+func pdfCreate(pdm map[string]string) error {
+	pdf := gofpdf.New("P", "mm", "A4", "") //crea il pdf
+	pdf.AddPage()                          //crea la pagina
+	pdf.SetFont("Arial", "B", 12)          //imposta il font
+	pdf.Cell(40, 10, "Giocatore")          //crea il Nome Giocatore
+	pdf.Cell(40, 10, "Personaggio")        //crea il Nome Personaggio
+	pdf.Ln(8)
+	pdf.SetFont("Arial", "", 10)
+	pdf.Cell(40, 10, pdm["Utente"]) // rimpie il nome giocatore
+	pdf.Cellf(40, 10, pdm["Nome"])  //	riempie il nome personaggio
+	pdf.Ln(8)
+	pdf.SetFont("Arial", "B", 12) //imposta il font
+	pdf.Cell(40, 10, "Razza")     //crea la razza
+	pdf.Cell(40, 10, "Genere")    //crea il genere
+	pdf.Ln(8)                     //a capo (spaziatura normale)
+	pdf.SetFont("Arial", "", 10)
+	pdf.Cell(40, 10, pdm["Razza"])   // rimpie il la razza
+	pdf.Cell(40, 10, pdm["Genere"])  // rimpie il genere
+	pdf.Ln(8)                        //a capo (spaziatura normale)
+	pdf.SetFont("Arial", "B", 12)    //imposta il font
+	pdf.Cell(40, 10, "Allineamento") //crea l'allineamento
+	pdf.Cell(40, 10, "Taglia")       //crea la taglia
+	pdf.Cell(40, 10, "Classe")       //crea la classe
+	pdf.Cell(40, 10, "Divinita'")    //crea la divinità
+	pdf.Ln(8)
+	pdf.SetFont("Arial", "", 10)
+	pdf.Cell(40, 10, pdm["Allineamento"])                                         // rimpie l'allineamento
+	pdf.Cell(40, 10, pdm["Taglia"])                                               // rimpie la taglia
+	pdf.Cell(40, 10, pdm["Classe"])                                               // rimpie la classe
+	pdf.Cell(40, 10, pdm["Divinità"])                                             // rimpie la divinità
+	checkErrors(pdf.OutputFileAndClose("La scheda di " + pdm["Utente"] + ".pdf")) //salva il pdf
 	return nil
 }
 
 //legge il json
 func init() {
-	checkErrors(ReadFromJSON(&Conf, "conf.json"))
+	checkErrors(ReadFromJSON(&Conf, "../conf.json"))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl1 := template.Must(template.ParseFiles("dnd.html"))
+	tmpl1 := template.Must(template.ParseFiles("../dnd.html"))
 	homeMap := make(map[string]interface{})
 	homeMap["Razza"] = Conf.Razza
 	homeMap["Genere"] = Conf.Genere
-	tmpl1.Execute(w, homeMap)
+	checkErrors(tmpl1.Execute(w, homeMap))
 
 }
 
 func answerHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl2 := template.Must(template.ParseFiles("answer.html"))
+	tmpl2 := template.Must(template.ParseFiles("../answer.html"))
+	processMap := make(map[string]string) //mappa per salvare i parametri
 
-	//array di stringhe in cui vengono salvate le scelte dell'utente
-	selezioni := []string{r.FormValue("firstname"), r.FormValue("genere"), r.FormValue("razza")}
-
-	processMap := make(map[string]interface{}) //mappa per salvare i parametri
-	processMap["Utente"] = selezioni[0]
-	convertiGenere, _ := strconv.Atoi(selezioni[1]) //funzione che converte in interi i valori di selezioni
+	processMap["Utente"] = r.FormValue("firstname")
+	convertiGenere, _ := strconv.Atoi(r.FormValue("genere"))
+	convertiRazza, _ := strconv.Atoi(r.FormValue("razza"))
+	selezioni := []int{convertiGenere, convertiRazza}
 	processMap["Genere"] = Conf.Genere[convertiGenere]
-	convertiRazza, _ := strconv.Atoi(selezioni[2]) //funzione che converte in interi i valori di selezioni
 	processMap["Razza"] = Conf.Razza[convertiRazza]
-	personaggio := new(Personaggio) //crea l'oggetto personaggio
-	//inserisce le key dei valori scelti dall'utente dentro p.selection, per poter generare il nome adatto
-	personaggio.Selection = []int{convertiGenere, convertiRazza}
 
-	personaggio.Genera()
-	processMap["Nome"] = personaggio.NomePersonaggio
-	processMap["Allineamento"] = personaggio.Allineamento
-	processMap["Taglia"] = personaggio.Taglia
-	processMap["Classe"] = personaggio.Classe
-	processMap["Divinita"] = personaggio.Dio
+	Genera(processMap, selezioni)
 
-	tmpl2.Execute(w, processMap)
+	checkErrors(tmpl2.Execute(w, processMap))
 
-	pdf := gofpdf.New("P", "mm", "A4", "") //crea il pdf
-	pdf.AddPage()                          //crea la pagina
-	pdf.SetFont("Arial", "B", 12)          //imposta il font
-	pdf.Cell(40, 10, "Giocatore")          //crea la Nome Giocatore
-	pdf.Cell(40, 10, "Personaggio")        //crea la Nome Personaggio
-	pdf.Ln(8)
-	pdf.SetFont("Arial", "", 10)
-	pdf.Cell(40, 10, r.FormValue("firstname"))    //la seconda scritta è consecutiva alla prima
-	pdf.Cell(40, 10, personaggio.NomePersonaggio) //la seconda scritta è consecutiva alla prima
-	pdf.Ln(8)
-	pdf.SetFont("Arial", "B", 12) //imposta il font
-	pdf.Cell(40, 10, "Razza")     //crea la scritta
-	pdf.Cell(40, 10, "Genere")    //crea la scritta
-	pdf.Ln(8)                     //a capo (spaziatura normale)
-	pdf.SetFont("Arial", "", 10)
-	pdf.Cell(40, 10, Conf.Razza[convertiRazza])   //la seconda scritta è consecutiva alla prima
-	pdf.Cell(40, 10, Conf.Genere[convertiGenere]) //la seconda scritta è consecutiva alla prima
-	pdf.Ln(8)                                     //a capo (spaziatura normale)
-	pdf.SetFont("Arial", "B", 12)                 //imposta il font
-	pdf.Cell(40, 10, "Allineamento")              //crea la scritta
-	pdf.Cell(40, 10, "Taglia")                    //crea la scritta
-	pdf.Cell(40, 10, "Classe")                    //crea la scritta
-	pdf.Cell(40, 10, "Divinita'")                 //crea la scritta
-	pdf.Ln(8)
-	pdf.SetFont("Arial", "", 10)
-	pdf.Cell(40, 10, personaggio.Allineamento)           //la seconda scritta è consecutiva alla prima
-	pdf.Cell(40, 10, personaggio.Taglia)                 //la seconda scritta è consecutiva alla prima
-	pdf.Cell(40, 10, personaggio.Classe)                 //la seconda scritta è consecutiva alla prima
-	pdf.Cell(40, 10, personaggio.Dio)                    //la seconda scritta è consecutiva alla prima
-	pdf.OutputFileAndClose("LaMiaSchedaPersonaggio.pdf") //salva il pdf
-
+	checkErrors(pdfCreate(processMap))
 }
 
 func main() {
